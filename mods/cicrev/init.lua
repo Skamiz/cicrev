8e5740c50aed0b679cbe3d282faa5c86d894bea3
Skamiz <48406239+Skamiz@users.noreply.github.com>
2025-06-02 07:59:40 +0200

    raise stack limit

--- mods/cicrev/init.lua
+++ mods/cicrev/init.lua
@@ -14,7 +14,25 @@ dofile(modpath .. "/debug.lua")
 dofile(modpath .. "/growing_trees.lua")
 
 
+local function override_stack_limits(node_limit, item_limit)
+	for name, def in pairs(minetest.registered_nodes) do
+		if not rawget(def, "stack_max") then
+			minetest.override_item(name,{
+				stack_max = node_limit,
+			})
+		end
+	end
+	for name, def in pairs(minetest.registered_craftitems) do
+		if not rawget(def, "stack_max") then
+			minetest.override_item(name,{
+				stack_max = item_limit,
+			})
+		end
+	end
+end
+
 minetest.register_on_mods_loaded(function()
+	-- override_stack_limits(8, 16)
 	for name, def in pairs(minetest.registered_nodes) do
 		local groups = table.copy(def.groups)
 		groups["everything"] = 1
@@ -22,24 +40,11 @@ minetest.register_on_mods_loaded(function()
 			groups = groups,
 		})
 
-		if not rawget(def, "stack_max") then
-			minetest.override_item(name,{
-				stack_max = 8,
-			})
-		end
-
 		if def.drawtype == "normal" and def.walkable then
 			local groups = table.copy(def.groups)
 			groups["solid_node"] = 1
 			minetest.override_item(name,{
 				groups = groups,
-			})
-		end
-	end
-	for name, def in pairs(minetest.registered_craftitems) do
-		if not rawget(def, "stack_max") then
-			minetest.override_item(name,{
-				stack_max = 16,
 			})
 		end
 	end
