9cd8b034326a07b8ed5b3e9064d7fb0f2dafecd8
Skamiz <48406239+Skamiz@users.noreply.github.com>
2025-06-05 16:20:01 +0200

    add c_recipes mod

---
+++ mods/API/c_recipes/api.lua
@@ -0,0 +1,166 @@
+
+c_recipes.registered_recipes = {}
+c_recipes.recipes_by_input = {}
+c_recipes.recipes_by_output = {}
+c_recipes.recipes_by_group = {}
+
+local game_running = false
+core.after(0, function() game_running = true end)
+
+local bad_modname = "Recipe name must follow 'mod_name:recipe_name' format. Was instead '%s'"
+local already_registered = "Failed to register recipe as recipe '%s' already exists."
+c_recipes.register_recipe = function(name, def)
+	local mod_origin = core.get_current_modname()
+	local mod_name = name:match("(^%S+):%S+")
+	assert(mod_name == mod_origin, bad_modname:format(name))
+
+	if c_recipes.registered_recipes[name] then
+		core.log("warning", already_registered:format(name))
+		return false
+	end
+
+	def.name = name
+	def.mod_origin = mod_origin
+	def.image = def.image or next(def.inputs) or "blank.png"
+	def.description = def.description or ""
+	def.inputs = def.inputs or {}
+	def.outputs = def.outputs or {}
+	def.groups = def.groups or {}
+
+	c_recipes.registered_recipes[name] = def
+
+	if game_running then
+		c_recipes.resolve_recipe_aliases(def)
+		c_recipes.cache_recipe(def)
+	end
+
+	return true
+end
+
+local nonexistent_recipe = "Failed to unregister recipe '%s' as no such recipe is registered."
+c_recipes.unregister_recipe = function(name)
+	if not c_recipes.registered_recipes[name] then
+		core.log("warning", nonexistent_recipe:format(name))
+		return false
+	end
+
+	c_recipes.registered_recipes[name] = nil
+
+	if game_running then
+		c_recipes.regenerate_caches()
+	end
+
+	return true
+end
+
+-- c_recipes.get_recipes_by_inputs = function(inputs)
+-- 	return recipes
+-- end
+--
+-- c_recipes.get_recipes_by_outputs = function(outputs)
+-- 	return recipes
+-- end
+
+c_recipes.get_filtered_recipes = function(filter)
+	local recipes = {}
+	for name, def in pairs(c_recipes.registered_recipes) do
+		if filter(def) then
+			recipes[name] = def
+		end
+	end
+
+	return recipes
+end
+
+-- aliases
+-- TODO: test this
+c_recipes.resolve_alias = function(name)
+	while core.registered_aliases[name] do
+		name = core.registered_aliases[name]
+	end
+	return name
+end
+
+local nonexistent_item = "Recipe '%s' uses item '%s', which doesn't exist."
+c_recipes.resolve_recipe_aliases = function(recipe)
+	for input, amount in pairs(recipe.inputs) do
+		local is_group = input:find("^group:%S$")
+		local is_item = input:find("^%S:%S$")
+
+		if (not is_group) and is_item then
+			local new_input = c_recipes.resolve_alias(input)
+
+			recipe.inputs[new_input] = (recipe.inputs[new_input] or 0) + amount
+			recipe.inputs[input] = nil
+
+			if not core.registered_items[new_input] then
+				core.log("warning", nonexistent_item:format(recipe.name, new_input))
+			end
+		end
+	end
+	for output, amount in pairs(recipe.outputs) do
+		local is_group = output:find("^group:%S$")
+		local is_item = output:find("^%S:%S$")
+
+		if (not is_group) and is_item then
+			local new_output = c_recipes.resolve_alias(output)
+
+			recipe.outputs[new_output] = (recipe.outputs[new_output] or 0) + amount
+			recipe.outputs[output] = nil
+
+			if not core.registered_items[new_output] then
+				core.log("warning", nonexistent_item:format(recipe.name, new_output))
+			end
+		end
+	end
+end
+
+c_recipes.resolve_all_aliases = function()
+	for name, def in pairs(c_recipes.registered_recipes) do
+		c_recipes.resolve_recipe_aliases(def)
+	end
+end
+
+-- caching
+c_recipes.cache_recipe = function(recipe)
+	for input, amount in pairs(recipe.inputs) do
+		if not c_recipes.recipes_by_input[input] then
+			c_recipes.recipes_by_input[input] = {}
+		end
+		c_recipes.recipes_by_input[input][recipe.name] = recipe
+	end
+	for output, amount in pairs(recipe.outputs) do
+		if not c_recipes.recipes_by_output[output] then
+			c_recipes.recipes_by_output[output] = {}
+		end
+		c_recipes.recipes_by_output[output][recipe.name] = recipe
+	end
+	for group, value in pairs(recipe.groups) do
+		if not c_recipes.recipes_by_group[group] then
+			c_recipes.recipes_by_group[group] = {}
+		end
+		if value ~= 0 then
+			c_recipes.recipes_by_group[group][recipe.name] = recipe
+		end
+	end
+end
+
+c_recipes.regenerate_caches = function()
+	c_recipes.recipes_by_input = {}
+	c_recipes.recipes_by_output = {}
+	c_recipes.recipes_by_group = {}
+
+	for name, def in pairs(c_recipes.registered_recipes) do
+		c_recipes.cache_recipe(def)
+	end
+end
+
+
+local function finalize()
+	c_recipes.resolve_all_aliases()
+	c_recipes.regenerate_caches()
+end
+
+-- TODO: either or
+-- core.after(0, finalize)
+core.register_on_mods_loaded(finalize)
--- mods/API/c_recipes/init.lua
+++ mods/API/c_recipes/init.lua
@@ -1,3 +1,66 @@
-local modname = minetest.get_current_modname()
-local modpath = minetest.get_modpath(modname)
--- dofile(modpath .. "/foo.lua")
+local modname = core.get_current_modname()
+local modpath = core.get_modpath(modname)
+
+--[[
+Pure recipe API
+
+What is crafting???
+
+example recipes:
+
+craft wood and stone into a stone pickaxe
+craft wood and iron into an iron pickaxe at an anvil
+when crafting, consume staming/energy, whatever
+
+recipe def:{
+	name = "", -- unique string: "mod_name:recipe_name"
+	mod_origin = minetest.get_current_modname(),
+	image = "image.png", default to first output if not present
+	description = "",
+	inputs = {[itemname] = amount, ...},
+	outputs = {},
+	groups = {group_name = 4},
+	on_craft = func(crafter),
+	-- crafter may be player, or table with info about the machine that did it which should include a pos
+	maybe return value changes craft result?
+	_custom fields,
+}
+
+
+API
+register recipe
+unregister recipe
+get recipes by inputs --get recipes with matching input list
+get recipes by outputs -\\- output list
+register on craft(recipe, crafter)
+get filtered recipe list(filter func(recipe))
+
+
+c_recipes = {
+	registered_recipes = {},
+	--these are generated once after mods loaded then
+	--they need to be updated if recipes are added or removed during runtime
+	recipes_by_input = {
+		[itemname] = {[name] = def, ...}
+		[itemname] = {}
+	}
+	recipes_by_output = {
+		[itemname] = {}
+		[itemname] = {}
+	}
+	recipes_by_group = {
+		[groupname] = {}
+		group:crafting_anvil, group:crafting_loom, ... group:personal_recipe
+		group:personal_recipe, limited to that specific player, though this mod doesn't care about that, that falls to crafting mod
+	}
+}
+
+
+at the end, a validation step, to check that used items exist
+
+maybe! function to create a formspec that show a given recipe
+]]
+
+c_recipes = {}
+
+dofile(modpath .. "/api.lua")
